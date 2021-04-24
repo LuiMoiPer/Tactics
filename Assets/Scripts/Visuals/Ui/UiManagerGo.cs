80400938dc51ebd57428e0a59f23d424e250b0ab
LuiMoiPer <luimoiper@outlook.com>
2021-04-24 14:33:59 -0700

    Renamed UI manager and added GameManagersGo's functionallity to it.

---
+++ Assets/Scripts/Visuals/Ui/UiManagerGo.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages all the menus for the game, from start to action selection.
+/// </summary>
+public class UiManagerGo : MonoBehaviour {
+    GameManager gameManager;
+    GameObject highlightPrefab;
+    private GameObject selectionPrefab;
+    private GameObject selectionVisual;
+    Transform highlightParent;
+    RaycastHit hit;
+    Ray ray;
+    void Start() {
+        // setup non unity stuff
+        gameManager = new GameManager();
+        gameManager.onSelectedUnitChange += HandleSelectedUnitChange;
+        // grab prefabs
+        highlightPrefab = Resources.Load("Prefabs/Highlight") as GameObject;
+        selectionPrefab = Resources.Load("Prefabs/Selection") as GameObject;
+        // make game objects
+        highlightParent = new GameObject("HighlightParent").transform;
+        highlightParent.SetParent(gameObject.transform);
+
+        selectionVisual = GameObject.Instantiate(
+            selectionPrefab,
+            new Vector3(0, 0, 0),
+            Quaternion.identity,
+            gameObject.transform
+        );
+        selectionVisual.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            HandleClick();
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            PlayTurn();
+        }
+    }
+
+    public void PlayTurn() {
+        gameManager.PlayTurn();
+    }
+
+    public void Highlight(List<Coord> positions) {
+        ClearHighlight();
+        foreach (Coord position in positions) {
+            GameObject.Instantiate(
+                highlightPrefab,
+                new Vector3(position.x, 0, position.y),
+                Quaternion.identity,
+                highlightParent
+            );
+        }
+    }
+
+    private void ClearHighlight() {
+        foreach (Transform child in highlightParent) {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void HandleSelectedUnitChange(object sender, EventArgs args) {
+        ClearHighlight();
+        if (gameManager.selectedUnit != null) {
+            Highlight(gameManager.ValidPositions(gameManager.selectedUnit.neighboors));
+        }
+    }
+
+    private void HandleClick() {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        UnitGo unitGo = null;
+        if (Physics.Raycast(ray, out hit)
+            && hit.transform != null
+            && (bool) (unitGo = hit.transform.GetComponent<UnitGo>())
+        ) {
+            gameManager.Select(unitGo.GetUnit());
+        }
+    }
+}
