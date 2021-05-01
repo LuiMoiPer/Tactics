8fcb2162c479d941f03b2cd3b9d9237fc98df4be
LuiMoiPer <luimoiper@outlook.com>
2021-04-30 21:05:49 -0700

    Now adds units to the game manager

--- Assets/Scripts/SceneRunner.cs
+++ Assets/Scripts/SceneRunner.cs
@@ -10,13 +10,14 @@ public class SceneRunner : MonoBehaviour {
         UiManagerGo uiManagerGo = GameObject.Find("UiManager").GetComponent<UiManagerGo>();
         uiManagerGo.onGameManagerChanged += HandleGameManagerChanged;
         if (uiManagerGo.gameManager != null) {
-            gameManager = uiManagerGo.gameManager;
-            gameManager.grid = new Grid(8, 8);
+            HandleGameManagerChanged(uiManagerGo, EventArgs.Empty);
         }
     }
 
     private void HandleGameManagerChanged(object sender, EventArgs args) {
         gameManager = ((UiManagerGo) sender).gameManager;
         gameManager.grid = new Grid(8, 8);
+        gameManager.AddUnit(new Unit(new Coord(2, 2)));
+        gameManager.AddUnit(new Unit(new Coord(3, 7)));
     }
 }
