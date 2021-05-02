c80a348f6aa5fd037a350dbc79470854a85ede4f
LuiMoiPer <luimoiper@outlook.com>
2021-05-01 23:44:28 -0700

    Can now diaplay a list of actions

--- Assets/Scripts/Visuals/Ui/ActionListGo.cs
+++ Assets/Scripts/Visuals/Ui/ActionListGo.cs
@@ -1,22 +1,52 @@
 using UnityEngine;
-
+using TMPro;
 public class ActionListGo : MonoBehaviour {
+    private int horizontalSpacing = 1;
+    private int verticalSpacing = 1;
     private GameObject buttonPrefab;
 
     void Start() {
         GetPrefabs();
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = Vector2.zero;
     }
 
     public void ShowActions(ActionType[] actionTypes) {
+        ClearActions();
 
+        RectTransform rectTransform;
+        GameObject button;
+        float yPosition = -verticalSpacing;
+
+        foreach (ActionType actionType in actionTypes) {
+            rectTransform = MakeButton(actionType);
+            rectTransform.anchoredPosition = new Vector2(horizontalSpacing, yPosition);
+            yPosition -= rectTransform.rect.height + verticalSpacing;
+        }
     }
 
-    public void HideActions() {
-
+    public void ClearActions() {
+        foreach (Transform button in gameObject.transform) {
+            Destroy(button.gameObject);
+        }
     }
 
-    private void MakeButton(ActionType actionType) {
+    private RectTransform MakeButton(ActionType actionType) {
+        GameObject button = GameObject.Instantiate(
+            buttonPrefab,
+            gameObject.transform.position,
+            Quaternion.identity,
+            gameObject.transform
+        );
+        RectTransform rectTransform = button.GetComponent<RectTransform>();
+        rectTransform.anchorMax = new Vector2(0f, 1f);
+        rectTransform.anchorMin = new Vector2(0f, 1f);
+        rectTransform.pivot = new Vector2(0f, 1f);
 
+        TextMeshProUGUI text = button.GetComponentInChildren<TextMeshProUGUI>();
+        text.SetText(actionType.ToString());
+
+        return rectTransform;
     }
 
     private void GetPrefabs() {
